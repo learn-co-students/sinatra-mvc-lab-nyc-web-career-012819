3a7c5d6189cd041828cb3788ee154d0466daafd3
github username <github email address>
2019-02-19 09:44:31 -0500

    Done.

--- spec/models/piglatinizer_spec.rb
+++ spec/models/piglatinizer_spec.rb
@@ -1,6 +1,7 @@
+
+
 describe 'class PigLatinizer' do
   let!(:words) { PigLatinizer.new }
-
 
   it 'can create a new instance of a class' do
     expect(PigLatinizer.new).to be_an_instance_of(PigLatinizer)

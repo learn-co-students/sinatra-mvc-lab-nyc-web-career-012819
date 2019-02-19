efade54e0666cc4e950fe32cee6100d6b68170e0
github username <github email address>
2019-02-19 11:34:17 -0500

    refined

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,6 @@ class PigLatinizer
     text_arr = text.split(" ")
     changed_arr = []
     text_arr.map do |word|
-      len = word.downcase.count("a-z")
       first_letter = word[0].downcase
       if first_letter == "a" || first_letter =="e" || first_letter == "i" || first_letter == "o" || first_letter=="u"
         changed_arr << word + "way"
